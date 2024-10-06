79c2e14a87b687d51d2fcc41c33832b74efd2c65
Vizian <88489827+Vizian@users.noreply.github.com>
2024-10-06 02:32:22 +0200

    Update file_access.py

--- AgroVision/file_access.py
+++ AgroVision/file_access.py
@@ -1,44 +1,39 @@
 import earthaccess
 
 def download_data(short_name, bounding_box, date, download_path):
-    # Аутентификация через NASA Earthdata без передачи учетных данных
-    auth = earthaccess.login()  # используйте только login без аргументов
+    auth = earthaccess.login() 
 
-    # Поиск данных с начальным диапазоном дат
     results = earthaccess.search_data(
         short_name=short_name,
         temporal=(date, date),
         bounding_box=bounding_box
     )
 
-    # Проверка, найдены ли данные
     if results:
-        print(f"Найдено {len(results)} файлов. Загружаем...")
+        print(f"Foind {len(results)} files")
         files = earthaccess.download(results[0], download_path)
-        print("Данные успешно загружены.")
+        print("Downloaded")
         return files
 
-    # Если данные не найдены, попытка расширить поиск
-    print("Данные не найдены для указанного диапазона дат. Пытаемся расширить диапазон...")
 
-    # Расширение диапазона дат
-    new_start_date = "2024-01-01"  # Установите новую начальную дату (пример)
-    new_end_date = "2024-09-30"  # Установите новую конечную дату (пример)
 
-    # Повторный поиск с новым диапазоном дат
+    new_start_date = "2024-01-01"  
+    new_end_date = "2024-09-30" 
+
+
     results = earthaccess.search_data(
         short_name=short_name,
         temporal=(new_start_date, new_end_date),
         bounding_box=bounding_box
     )
 
-    # Проверка, найдены ли данные с новым диапазоном дат
+
     if results:
-        print(f"Найдено {len(results)} файлов в новом диапазоне дат. Загружаем...")
+        print(f"Found {len(results)} files")
         files = earthaccess.download(results, download_path)
-        print("Данные успешно загружены.")
+        print("Downloaded")
         return files
 
-    # Если все еще нет результатов, информируем пользователя
-    print("Данные не найдены для расширенного диапазона дат.")
-    return None  # Возвращаем None, если файлы не найдены
+
+    print("Couldn't find data")
+    return None
